2300cde6f568762b4eb19b045b7d284cf2c0d36c
abcbuzhiming <abcbuzhiming@gmail.com>
2020-07-07 17:40:14 +0800

    20200707 1740

--- sample-goframe/README.md
+++ sample-goframe/README.md
@@ -31,3 +31,29 @@ Windows powershell：	$env:GOPROXY = "https://goproxy.io"
 启动: go run main.go
 
 
+## 编译
+* 一般编译
+go build main.go
+
+* 交叉编译-Windows下输出到其它系统
+```bat
+SET CGO_ENABLED=0
+SET GOOS=darwin
+SET GOARCH=amd64
+go build main.go
+
+SET CGO_ENABLED=0
+SET GOOS=linux
+SET GOARCH=amd64
+go build main.go
+```
+
+
+* 交叉编译-Linux下输出其它系统
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build quick-start.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build quick-start.go
+
+* 交叉编译依赖 golang.org/x下包的问题解决
+https://shockerli.net/post/go-get-golang-org-x-solution/
+
+
--- sample-goframe/api/user.go
+++ sample-goframe/api/user.go
@@ -1,9 +1,9 @@
 package user
 
 import (
-    //"github.com/gogf/gf-demos/app/service/user"
     "github.com/gogf/gf-demos/library/response"
     "github.com/gogf/gf/net/ghttp"
+    "sample-goframe/service"
 )
 
 // 用户API管理对象
@@ -12,7 +12,7 @@ type Controller struct{}
 
 // 登录请求参数，用于前后端交互参数格式约定
 type SignInRequest struct {
-    UserName string `v:"required#账号不能为空"`
+    Passport string `v:"required#账号不能为空"`
     Password string `v:"required#密码不能为空"`
 }
 
@@ -22,12 +22,12 @@ func (c *Controller) SignIn(r *ghttp.Request) {
     if err := r.Parse(&data); err != nil {
         response.JsonExit(r, 1, err.Error())
 	}
-	/*
+	
     if err := user.SignIn(data.Passport, data.Password, r.Session); err != nil {
         response.JsonExit(r, 1, err.Error())
     } else {
         response.JsonExit(r, 0, "ok")
 	}
-	*/
+	
 	response.JsonExit(r, 0, "ok")
 }
